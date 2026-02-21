5e10163be6ae59f01d458785fc4a6bf2735d5519
ksco <1375926565@qq.com>
2026-02-21 20:02:50 +0100

    fix: remove custom entry.server.jsx, use React Router default SSR entry

--- src/routes/entry.server.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ServerRouter } from 'react-router';
-
-export default function EntryServer({ context, url }) {
-    return (
-        <ServerRouter context={context} url={url} />
-    );
-}
